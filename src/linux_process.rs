a94353889b22ceb4ad61e85167b984a05f1f6748
Reinis Muiznieks <muiznieks.reinis@gmail.com>
2019-05-04 21:16:10 +0100

    Fixed zoom hack to work with rust

---
+++ src/linux_process.rs
@@ -0,0 +1,146 @@
+use failure::{bail, Error};
+use goblin::elf::sym::Sym;
+use nix::sys::ptrace::AddressType;
+use proc_maps::{get_process_maps, MapRange, Pid};
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+use std::os::raw::c_void;
+
+pub fn get_process_id_by_name(name: String) -> Result<Pid, Error> {
+    let entries = fs::read_dir("/proc")?;
+
+    for entry in entries {
+        let entry = entry?;
+        if !entry.file_type()?.is_dir() {
+            continue;
+        }
+        let mut comm_path = entry.path();
+        comm_path.push("comm");
+
+        if !comm_path.exists() {
+            continue;
+        }
+
+        let output = fs::read(comm_path.as_path())?;
+        let output = String::from_utf8(output)?;
+
+        if output.replace("\n", "") == name {
+            let pid = match entry.file_name().into_string() {
+                Ok(pid) => pid,
+                Err(e) => bail!("Cannot parse entry: {:?}", e),
+            };
+            let pid = pid.parse::<Pid>()?;
+            return Ok(pid);
+        }
+    }
+    bail!("Unable to find process named: {}", name)
+}
+
+pub fn get_process_map_range(pid: Pid, pred: fn(&MapRange) -> bool) -> Result<MapRange, Error> {
+    let maps = get_process_maps(pid)?;
+    for map in maps {
+        if pred(&map) && map.is_exec() {
+            println!(
+                "Found module: Filename {:?} Address {} Size {} - {}",
+                map.filename(),
+                map.start(),
+                map.size(),
+                map.is_exec(),
+            );
+            return Ok(map);
+        }
+    }
+    bail!("Unable to find symbols for process with id: {}", pid)
+}
+
+pub fn get_symbol_map(binary_path: &String) -> Result<HashMap<String, Sym>, Error> {
+    let mut f = File::open(binary_path)?;
+    let mut buf = Vec::new();
+    f.read_to_end(&mut buf).unwrap();
+    let bytes = buf.as_slice();
+
+    Ok(get_symbol_map_from_bytes(bytes))
+}
+
+pub fn attach(pid: Pid) -> Result<(), Error> {
+    use nix::sys::ptrace::attach;
+    let pid = nix::unistd::Pid::from_raw(pid);
+    attach(pid)?;
+
+    Ok(())
+}
+
+pub fn detach(pid: Pid) -> Result<(), Error> {
+    use nix::sys::ptrace::detach;
+    let pid = nix::unistd::Pid::from_raw(pid);
+    detach(pid)?;
+
+    Ok(())
+}
+
+pub fn waitpid(pid: Pid) -> Result<(), Error> {
+    use nix::sys::wait::waitpid;
+    let pid = nix::unistd::Pid::from_raw(pid);
+    waitpid(pid, None)?;
+
+    Ok(())
+}
+
+pub fn write_process_memory(pid: Pid, addr: usize, buffer: Vec<u8>) -> Result<(), Error> {
+    use byteorder::{ByteOrder, LittleEndian};
+    use nix::sys::ptrace::read;
+    use nix::sys::ptrace::write;
+
+    let pid = nix::unistd::Pid::from_raw(pid);
+
+    let pointer_size = std::mem::size_of::<AddressType>();
+    let mut address = addr as AddressType;
+
+    for chunk in buffer.chunks(pointer_size) {
+        if chunk.len() == pointer_size {
+            let data = LittleEndian::read_uint(chunk, pointer_size);
+            write(pid, address, data as *mut c_void)?;
+            println!("{:x?} Wrote: {:x?}", address, data);
+        } else {
+            let cur_data = read(pid, address)?;
+            let slice = unsafe { any_as_u8_slice(&cur_data) };
+            println!("{:x?} read in data: {:x?}", address, slice);
+            for (i, b) in chunk.iter().enumerate() {
+                slice[i] = *b;
+            }
+
+            let data = LittleEndian::read_uint(slice, pointer_size);
+
+            write(pid, address, data as *mut c_void)?;
+            println!("{:x?} Wrote at end: {:x?}", address, data);
+        }
+        address = (addr as usize + pointer_size) as AddressType;
+    }
+
+    Ok(())
+}
+
+unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &mut [u8] {
+    ::std::slice::from_raw_parts_mut((p as *const T) as *mut u8, ::std::mem::size_of::<T>())
+}
+
+fn get_symbol_map_from_bytes(bytes: &[u8]) -> HashMap<String, Sym> {
+    let mut symbol_map = HashMap::new();
+
+    let parsed_binary = goblin::elf::Elf::parse(&bytes);
+    if let Ok(binary) = parsed_binary {
+        let elf = binary as goblin::elf::Elf;
+        let syms = elf.dynsyms.to_vec();
+        let strtab = elf.dynstrtab;
+        for sym in syms {
+            if let Some(Ok(symbol_name)) = strtab.get(sym.st_name) {
+                symbol_map.insert(symbol_name.to_string(), sym);
+            }
+        }
+    } else {
+        println!("Failed parsing elf {:?}", parsed_binary);
+    }
+    symbol_map
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,110 @@
-mod linux_process {
-    use std::fs;
+mod linux_process;
+use failure::{bail, Error};
+use goblin::elf::sym::Sym;
+use proc_maps::Pid;
+use std::collections::HashMap;
 
-    pub fn get_process_id_by_name(name: String) -> Option<u32> {
-        if let Ok(entries) = fs::read_dir("/proc") {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    if let Ok(file_type) = entry.file_type() {
-                        if !file_type.is_dir() {
-                            continue;
-                        }
-                    }
-                    let mut comm_path = entry.path();
-                    comm_path.push("comm");
+const ZOOM_OUT: &str = "_ZN7CPlayer7ZoomOutEv";
+const PREPARE_CLIENT_STATE: &str = "_ZN7CPlayer18PrepareClientStateER12CClientStateb";
+const SETUP_CAMERA: &str = "_ZN7CPlayer11SetupCameraER7CCameraRK5CVec3IfES5_";
 
-                    let output = fs::read(comm_path.as_path());
-                    if let Ok(output) = output {
-                        if let Ok(output) = String::from_utf8(output) {
-                            if output.replace("\n", "") == name {
-                                if let Ok(process_id) = entry.file_name().into_string() {
-                                    return Some(process_id.parse::<u32>().unwrap());
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+fn zoom_hack() -> Result<(), Error> {
+    let process_name = "hon-x86_64";
+    let p_id = linux_process::get_process_id_by_name(process_name.to_string())?;
+
+    println!("Found process_id for {}: {}", process_name, p_id);
+    let map_range = linux_process::get_process_map_range(p_id, |map| {
+        map.filename()
+            .clone()
+            .unwrap_or_else(|| "".to_string())
+            .contains("shared")
+    })?;
+    let filename = map_range.filename().clone().unwrap();
+
+    let symbol_map = linux_process::get_symbol_map(&filename)?;
+    println!("ZOOM_OUT {:?}", symbol_map.get(ZOOM_OUT));
+
+    linux_process::attach(p_id)?;
+    linux_process::waitpid(p_id)?;
+
+    println!("Attached");
+    let result = patch(p_id, map_range.start(), &symbol_map);
+
+    match result {
+        Ok(_) => {
+            linux_process::detach(p_id)?;
+            println!("Patched");
         }
-        None
+        Err(e) => {
+            linux_process::detach(p_id)?;
+            bail!("Error: {:?}", e);
+        }
     }
+
+    Ok(())
+}
+
+fn patch(pid: Pid, module_offset: usize, symbol_map: &HashMap<String, Sym>) -> Result<(), Error> {
+    let zoom_out_offset = symbol_map.get(ZOOM_OUT).unwrap().st_value as usize;
+    let prepare_client_state_offset =
+        symbol_map.get(PREPARE_CLIENT_STATE).unwrap().st_value as usize;
+    let setup_camera_offset = symbol_map.get(SETUP_CAMERA).unwrap().st_value as usize;
+
+    // ZoomOut + 0x4f (minss   xmm1, xmm2) -> (movss   xmm1, xmm2)
+    linux_process::write_process_memory(
+        pid,
+        zoom_out_offset + module_offset + 0x4f,
+        vec![0xF3, 0x0F, 0x10, 0xCA],
+    )?;
+
+    // ZoomOut + 0x5e (minss   xmm1, xmm3) -> (movss   xmm1, xmm3)
+    linux_process::write_process_memory(
+        pid,
+        zoom_out_offset + module_offset + 0x5e,
+        vec![0xF3, 0x0F, 0x10, 0xCB],
+    )?;
+
+    // PrepareClientState + 0x222 (movss   cs:currentCammeraZoom, xmm0) -> (NOP, NOP ..)
+    linux_process::write_process_memory(
+        pid,
+        prepare_client_state_offset + module_offset + 0x222,
+        vec![0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90],
+    )?;
+
+    //PrepareClientState + 0xc78 (minss   xmm2, xmm3 ) -> (movss   xmm2, xmm3)
+    linux_process::write_process_memory(
+        pid,
+        prepare_client_state_offset + module_offset + 0xc78,
+        vec![0xF3, 0x0F, 0x10, 0xD3],
+    )?;
+
+    //PrepareClientState + 0xc91 (ja      short loc_C9847C ) -> (jmp      short loc_C9847C)
+    linux_process::write_process_memory(
+        pid,
+        prepare_client_state_offset + module_offset + 0xc91,
+        vec![0x90, 0x90],
+    )?;
+
+    //PrepareClientState + 0xc93 (minss   xmm1, xmm2) -> movss   xmm1, xmm2)
+    linux_process::write_process_memory(
+        pid,
+        prepare_client_state_offset + module_offset + 0xc93,
+        vec![0xF3, 0x0F, 0x10, 0xCA],
+    )?;
+
+    //SetupCamera + 0x5df (jbe     short loc_C97702 ) -> (jmp     short loc_C97702)
+    linux_process::write_process_memory(
+        pid,
+        setup_camera_offset + module_offset + 0x5df,
+        vec![0xEB],
+    )?;
+
+    Ok(())
 }
 
 fn main() {
-    let process_name = "hon";
-    let process_id = linux_process::get_process_id_by_name(process_name.to_string());
-
-    if let Some(ref p_id) = process_id {
-        println!("Found process_id for {}: {}", process_name, p_id);
-    } else {
-        println!("Failed to find process_id for {}", process_name);
+    match zoom_hack() {
+        Ok(_) => println!("Succeeded!"),
+        Err(e) => println!("Failed {:?}", e),
     }
 }
